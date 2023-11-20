aa5f7bd8b8a9e9a7e4780473364e6da9645c71bf
Leon Bubova <bubova.leon@gmx.de>
2023-11-20 23:31:04 +0100

    fix layout on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Testimonials } from "./components/testimony";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-8 pb-8 md:gap-16 md:pb-16 xl:pb-24">
+    <div className="flex flex-col gap-8 pb-8 md:gap-16 md:pb-16 xl:pb-24 mt-40 md:mt-0">
       <div className="flex flex-col items-center justify-center max-w-3xl px-8 mx-auto mt-8 sm:min-h-screen sm:mt-0 sm:px-0">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <Link
@@ -29,7 +29,7 @@ export default function Home() {
             {/*</Link>*/}
             <Link
               href="/format"
-              className="sm:w-1/2 sm:text-center inline-block transition-all space-x-2  rounded px-4 py-1.5 md:py-2 text-base font-semibold leading-7 text-zinc-800   bg-zinc-50 ring-1 ring-transparent hover:text-zinc-100 hover:ring-zinc-600/80  hover:bg-zinc-900/20 duration-150 hover:drop-shadow-cta"
+              className="sm:w-1/2 flex justify-between sm:justify-center transition-all space-x-2  rounded px-4 py-1.5 md:py-2 text-base font-semibold leading-7 text-zinc-800   bg-zinc-50 ring-1 ring-transparent hover:text-zinc-100 hover:ring-zinc-600/80  hover:bg-zinc-900/20 duration-150 hover:drop-shadow-cta"
             >
               <span>Try it</span>
               <span aria-hidden="true">&rarr;</span>
