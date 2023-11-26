f4e716f9208d19bb46c799d18ed54dca019756e9
Leon Bubova <bubova.leon@gmx.de>
2023-11-26 21:52:53 +0100

    fix header

--- app/head.tsx
+++ app/head.tsx
@@ -43,8 +43,6 @@ export default function Head({title, subtitle}: {
             <meta property="twitter:title" content={title}/>
             <meta property="twitter:description" content={subtitle}/>
             <meta property="twitter:image" content={url.toString()}/>
-
-            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         </>
     );
 }
